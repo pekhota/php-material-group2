c99e69fba5ff380884cab722135801d56512cac6
Alexander Pekhota <alex@pdffiller.com>
2021-12-10 23:46:19 +0200

    lesson 8 part 2

--- lesson-content/simple-site-demo/layout/content.php
+++ lesson-content/simple-site-demo/layout/content.php
@@ -1,4 +1,4 @@
-<main class="container">
+
     <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
             <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
@@ -172,5 +172,3 @@
         require __DIR__."/sidebar.php";
         ?>
     </div>
-
-</main>
---
+++ lesson-content/simple-site-demo/layout/subscribe.php
@@ -0,0 +1,9 @@
+<form action="/subscribe" method="post" class="row g-3">
+    <div class="col-auto">
+        <label for="staticEmail2" class="visually-hidden">Email</label>
+        <input name="email" type="text" class="form-control" id="staticEmail2" value="email@example.com">
+    </div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3">Subscribe</button>
+    </div>
+</form>
